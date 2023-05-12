57b34f253f3a15e674ce36506f5920d729236263
Abbas Sepahvand <abbassep@outlook.com>
2023-05-12 16:31:16 +0330

    update

--- src/layouts/mainLayout/top-nav.jsx
+++ src/layouts/mainLayout/top-nav.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import ChangeLanguage from "../../components/change-language";
 import ChangeTheme from "../../components/change-theme";
 import { useAppContext } from "../../contexts/app/app-context";
 
 const TopNav = () => {
     const {toggleSidebar} = useAppContext();
+    const {language} = useAppContext();
+    const navigate = useNavigate();
+
+    const logout = () => {
+      localStorage.removeItem('token');
+      navigate('/login');
+    }
     return (
         <nav className="navbar">
           <a
@@ -12,9 +20,14 @@ const TopNav = () => {
           >
             <i className="hamburger align-self-center"></i>
           </a>
-          <div className="d-flex align-items-center gap-3 ms-auto me-3">
+          <div className="d-flex align-items-center gap-3  me-3">
             <ChangeLanguage />
             <ChangeTheme />
+          </div>
+          <div className={`${language === 'fa' ? 'me-auto' : 'ms-auto'}`}>
+            <button className="btn ms-2 btn-outline-danger fw-bolder" onClick={logout}>
+              خارج شوید
+            </button>
           </div>
         </nav>
     )
